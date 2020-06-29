3753e03e7dee9a8ffdf0854b8c4e3ec6c2b94cc7
rdehaggart1 <rdeh10@googlemail.com>
2020-06-29 22:28:44 +0100

    logo change; head js; fleshing
    changed the logo slighly; added some head js for modularity; continued fleshing out the components section

---
+++ includes/head.js
@@ -0,0 +1,4 @@
+document.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+document.write("<link href=\"StyleSheet.css\" rel=\"stylesheet\">");
+document.write("<script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>");
+document.write("<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>");
