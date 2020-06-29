c2b37590176f97c2e65c8371033616169d609fff
rdehaggart1 <rdeh10@googlemail.com>
2020-06-29 20:04:27 +0100

    cleanup
    some rearranging & fleshing out the components&tools page

--- includes/header.js
+++ includes/header.js
@@ -1,3 +1,3 @@
-document.write("<a href = \"index.html\"><img class = \"headerImg\" src = \"logo.png\"></img></a>");
+document.write("<a href = \"index.html\"><img class = \"headerImg\" src = \"images\\logo.png\"></img></a>");
 document.write("<h3>Guitar FX Guide</h3>");
 document.write("<a href = \"about.html\" class = \"contactTab\">About</a></div>");
