2e7631fd0df5ff233cf404efb69db1aab9a88213
rdehaggart1 <rdeh10@googlemail.com>
2020-06-29 11:23:20 +0100

    javascript sidebar working!

---
+++ includes/sidebar.js
@@ -0,0 +1,9 @@
+document.write("<a style=\"font-weight:bold;color:orange;\">Navigation</a>");
+document.write("<HR class = \"orangeRow\">");
+document.write("<a href = \"index.html\">Introduction</a>");
+document.write("<a href = \"usefulOnlineResources.html\">Useful Online Resources</a>");
+document.write("<a href = \"safetyNotice.html\">Safety Notice</a>");
+document.write("<a href = \"whatIsASignal.html\">What is a Signal?</a>");
+document.write("<a href = \"simplifyingAssumptions.html\">Simplifying Assumptions</a>");
+document.write("<a href = \"importantConcepts.html\">Important Concepts</a>");
+document.write("<a href = \"componentsAndTools.html\">Components and Tools</a>");
