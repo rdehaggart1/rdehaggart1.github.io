ac11d16ef20fcca778b0b4d2ac816a48ce5e392c
rdehaggart1 <rdeh10@googlemail.com>
2020-06-29 17:18:37 +0100

    added about tab to header
    also fleshing out the 'about' page. about the author, contact details, etc.

--- includes/header.js
+++ includes/header.js
@@ -1,2 +1,3 @@
 document.write("<img class = \"headerImg\" src = \"logo.png\"></img>");
 document.write("<h3>Guitar FX Guide</h3>");
+document.write("<a href = \"about.html\" class = \"contactTab\">About</a></div>");
