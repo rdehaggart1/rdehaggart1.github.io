c00040c21c937db03c7833f242615c737c32ee94
rdehaggart1 <rdeh10@googlemail.com>
2020-06-30 17:28:31 +0100

    icon fix;
    icon call was in 'header' not 'head'... doh

--- includes/header.js
+++ includes/header.js
@@ -1,4 +1,3 @@
-document.write("<link rel = \"shortcut icon\" href = \"images/icon.ico\">");
 document.write("<a href = \"index.html\"><img class = \"headerImg\" src = \"images/logo.png\"></img></a>");
 document.write("<h3>Guitar Effects Guide</h3>");
 document.write("<a href = \"about.html\" class = \"contactTab\">About</a></div>");
