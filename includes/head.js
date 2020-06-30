ce36aafdbad86aa3fbd84391fc3d6988e259871b
rdehaggart1 <rdeh10@googlemail.com>
2020-06-30 17:29:46 +0100

    actually added icon...

--- includes/head.js
+++ includes/head.js
@@ -2,4 +2,4 @@ document.write("<meta name=\"viewport\" content=\"width=device-width, initial-sc
 document.write("<link href=\"StyleSheet.css\" rel=\"stylesheet\">");
 document.write("<script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>");
 document.write("<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>");
-document.write("<link rel = \"shortcut icon\" href = \"images/icon.ico\">");
+document.write("<link rel = \"icon\" href = \"images/icon.ico\">");
