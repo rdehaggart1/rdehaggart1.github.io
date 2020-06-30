37ec4e789c501ef71a09a04ca0d8f83cee4a945f
rdehaggart1 <rdeh10@googlemail.com>
2020-06-30 17:26:55 +0100

    icon; fleshing
    adding info about grounding; added an icon for the tab (sort of works...)

--- includes/header.js
+++ includes/header.js
@@ -1,3 +1,4 @@
-document.write("<a href = \"index.html\"><img class = \"headerImg\" src = \"images\\logo.png\"></img></a>");
+document.write("<link rel = \"shortcut icon\" href = \"images/icon.ico\">");
+document.write("<a href = \"index.html\"><img class = \"headerImg\" src = \"images/logo.png\"></img></a>");
 document.write("<h3>Guitar Effects Guide</h3>");
 document.write("<a href = \"about.html\" class = \"contactTab\">About</a></div>");
