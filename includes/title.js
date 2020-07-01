43eea5c2ee53948c965565f54bfbdad2a2723ae2
rdehaggart1 <rdeh10@googlemail.com>
2020-07-01 14:42:06 +0100

    bug fix

--- includes/title.js
+++ includes/title.js
@@ -1,3 +1,3 @@
 document.write("<h1>Understanding And Designing Guitar Effects Pedals</h1>");
 document.write("<h2>A Beginners Guide</h2>");
-document.write("<HR class = "orangeRow">");
+document.write("<HR class = \"orangeRow\">");
