4a4397a198d013ddd9ca1c819eb31b7778760565
rdehaggart1 <rdeh10@googlemail.com>
2020-07-04 11:59:30 +0100

    updated header

--- includes/header.js
+++ includes/header.js
@@ -1,3 +1,2 @@
 document.write("<a href = \"index.html\"><img class = \"headerImg\" src = \"images/logo.png\"></img></a>");
-document.write("<h3>Guitar Effects Guide</h3>");
-document.write("<a href = \"about.html\" class = \"contactTab\">About</a></div>");
+document.write("<a href = \"about.html\" class = \"headerTab\">About</a>");
